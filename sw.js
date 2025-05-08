40dc3c2824809dfa48e1af80ffcc325c09dc96be
Echo Reader <tom.bisnis@gmail.com>
2025-05-08 15:04:44 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js
+const CACHE_NAME = 'my-blog-cache-v1';
+const ASSETS = [
+  '/',
+  '/favicon.ico',
+  '/css/blog.css',
+  '/images/logo.png',
+  // Tambahkan URL aset yang ingin di-cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
